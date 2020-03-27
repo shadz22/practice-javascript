4eff65b6fceda7bcb573f4069afd3e9697ff42fd
shadz22 <shadz22@yahoo.co.uk>
2020-03-27 16:02:44 +0000

    Type coercion

---
+++ script.js
@@ -0,0 +1,9 @@
+var firstName = 'Shadi';
+var age = 38;
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+
+console.log(firstName + 'is a ' + age + ' year old ' + job + '. Is she married? ' + isMarried);
