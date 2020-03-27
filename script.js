a45f91272b99d7e664466cb95e56bd5609486819
shadz22 <shadz22@yahoo.co.uk>
2020-03-27 17:04:00 +0000

    Variable mutation

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-console.log("It's the same")
-
 // Type coercion
-
 var firstName = 'Shadi';
 var age = 38;
 console.log(firstName + ' ' + age);
@@ -12,3 +9,11 @@ isMarried = false;
 
 console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is she married? ' + isMarried);
 
+// variable mutation
+age = 'thirty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' years old ' + job + '. Is she married? ' + isMarried);
+
+var lastName = prompt("What is her last name?");
+console.log(firstName + ' ' + lastName);
