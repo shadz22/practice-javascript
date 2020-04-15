488439634852697ac357bc7bab9af4b857c07dae
shadz22 <shadz22@yahoo.co.uk>
2020-04-15 18:13:52 +0100

    Primitives vs Objects

--- Advanced Javascript - 5/script.js
+++ Advanced Javascript - 5/script.js
@@ -29,3 +29,42 @@ Person.prototype.lastName = "Smith";
 console.log(john);
 console.log(mark);
 console.log(jane);
+
+// Primitives vs Objects
+
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+
+//Objects
+
+var obj1 = {
+    name: "John",
+    age: 26
+}
+
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+
+// Functions
+
+var age = 27;
+var obj = {
+    name: "James",
+    city: "London"
+};
+
+function change(a, b) {
+  a = 30;
+  b.city = "Lisbon"
+}
+
+change(age, obj);
+console.log(age);
+console.log(obj.city);
